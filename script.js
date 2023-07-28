901cc4d7283ef1eeb4ac3c028d6d80720bb5e462
LuizFelipeBotiniSilvestro <luizfelipebssideropolis@gmail.com>
2023-07-28 19:55:11 -0300

    Animation menu

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+// Declare consts.
+const title = document.querySelector('.typing');
+const activeMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .primary-navigation')
+
+// Functions
 function activateLyrics(element) {
-  const arrayText = element.innerHTML.split('');
-  element.innerHTML = '';
-  arrayText.forEach((letter, i)=> {
-    setTimeout(()=> {
-      element.innerHTML += letter; 
-    }, 75 * i)
-  });
+    const arrayText = element.innerHTML.split('');
+    element.innerHTML = '';
+    arrayText.forEach((letter, i)=> {
+      setTimeout(()=> {
+        element.innerHTML += letter; 
+      }, 75 * i)
+    });
 }
 
-const title = document.querySelector('.typing');
+// Events
+activeMenu.addEventListener('click', () => {
+  activeMenu.classList.toggle('fa-x');
+  navMenu.classList.toggle('activated');
+})
+
+// To call functions
 activateLyrics(title);
+
+
+
+
